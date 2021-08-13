b376a045cab39266dfafd9115149ca4f9bbf01b4
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-08-13 16:57:18 +0900

    플레이어목록 UI 완료 옵저버 작업중

--- AirHelper/TabViews/Create/WaitingRoom.swift
+++ AirHelper/TabViews/Create/WaitingRoom.swift
@@ -42,34 +42,44 @@ struct WaitingRoom: View {
             VStack(alignment: .center, spacing: 0.5){
                 //레드팀, 블루팀 구별
                 HStack(alignment: .center, spacing: 3){
-                    HStack(){
-                        Image("sword")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: gp.size.width / 17)
-                        Text("RED TEAM")
-                            .fontWeight(.bold)
-                            .font(.system(size: 25))
+                    Button(action: {
+                        print("레드팀")
+                    }){
+                        
+                        HStack(){
+                            Image("sword")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: gp.size.width / 17)
+                            Text("RED TEAM")
+                                .fontWeight(.bold)
+                                .font(.system(size: 25))
+                        }
+                        .frame(width: gp.size.width*0.47, height: gp.size.height/10, alignment: .center)
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(5)
                     }
-                    .frame(width: gp.size.width*0.47, height: gp.size.height/10, alignment: .center)
-                    .background(Color.red)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(5)
-                    
-                    HStack(){
-                        Image("shield")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: gp.size.width / 17)
-                        Text("BLUE TEAM")
-                            .fontWeight(.bold)
-                            .font(.system(size: 25))
+                    Button(action: {
+                        print("블루팀")
+                    }){
+                        HStack(){
+                            Image("shield")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: gp.size.width / 17)
+                            Text("BLUE TEAM")
+                                .fontWeight(.bold)
+                                .font(.system(size: 25))
+                        }
+                        .frame(width: gp.size.width*0.47, height: gp.size.height/10, alignment: .center)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(5)
                     }
-                    .frame(width: gp.size.width*0.47, height: gp.size.height/10, alignment: .center)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(5)
                 }
+                
+                
                 //마크, 콜사인, 상태 구분
                 HStack(alignment: .center, spacing: 3){
                     HStack(){
@@ -98,6 +108,43 @@ struct WaitingRoom: View {
                     .foregroundColor(Color.white)
                     .cornerRadius(5)
                 }
+                //플레이어 목록
+                HStack(alignment: .center, spacing: 3){
+                    HStack(){
+                        List(){
+                            HStack(){
+                                Text(Image(systemName: "crown.fill"))
+                                    .foregroundColor(Color.yellow)
+                                    .frame(width: gp.size.width*0.1)
+                                Text("MAD")
+                                    .frame(width: gp.size.width*0.2, alignment: .leading)
+                                Text(Image(systemName: "checkmark"))
+                                    .frame(width: gp.size.width*0.1, alignment: .center)
+                            }
+                            .frame(width: gp.size.width*0.47, alignment: .leading)
+                        }
+                        .frame(width: gp.size.width*0.47, height: gp.size.height / 2, alignment: .center)
+                    }
+                    
+                    HStack(){
+                        List(){
+                            HStack(){
+                                Text(Image(systemName: "crown.fill"))
+                                    .foregroundColor(Color.yellow)
+                                    .frame(width: gp.size.width*0.1)
+                                Text("MAD")
+                                    .frame(width: gp.size.width*0.2, alignment: .leading)
+                                Text(Image(systemName: "checkmark"))
+                                    .frame(width: gp.size.width*0.1, alignment: .center)
+                            }
+                            .frame(width: gp.size.width*0.47, alignment: .leading)
+                            
+                        }
+                        .frame(width: gp.size.width*0.47, height: gp.size.height / 2, alignment: .center)
+                    }
+                }
+                .frame(width: gp.size.width, alignment: .leading)
+                
             }
             .frame(width: gp.size.width, height: gp.size.height, alignment: .center)
         }
