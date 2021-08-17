964272ef0d49e2264b3e5082341540d06f5dfe75
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-08-18 00:03:29 +0900

    대기룸 플레이어 리스트 완료

--- AirHelper/TabViews/Create/WaitingRoom.swift
+++ AirHelper/TabViews/Create/WaitingRoom.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+struct ResData: Codable {
+    let type: String
+    let data: [AttendUser]
+}
+
+struct AttendUser: Codable {
+    var id: Int
+    var user: User
+    var team: String
+    var is_admin: Bool
+    var room: Int
+}
+struct User: Codable {
+    var id: Int
+    var user_id: String
+    var email: String
+    var name: String
+    var call_sign: String
+    var profile_image: String
+}
+
 final class RoomModel: ObservableObject {
     private var webSocketTask: URLSessionWebSocketTask? // 1
     var room_id: Int = 0
     var is_admin = "attend"
+
+    @Published var attend_user_list: [AttendUser] = []
+    
     // MARK: - Connection
     func connect() { // 2
         let url = URL(string: "ws://airhelper.kro.kr/ws/\(is_admin)/\(room_id)/")! // 3
@@ -33,12 +57,29 @@ final class RoomModel: ObservableObject {
             print("Error", error)
         }
     }
+    
+    private func onMessage(message: URLSessionWebSocketTask.Message) {
+            if case .string(let text) = message {
+                if let data = text.data(using: .utf8) {
+                    let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
+                    
+                    if json["type"] as! String == "user_attend" {
+                        let decoder = JSONDecoder()
+                        if let test = try? decoder.decode(ResData.self, from: data) {
+                           
+                            DispatchQueue.main.async { // 6
+                                self.attend_user_list = test.data
+                            }
+                            
+                        }
+                    }
+                }
+                else {
+                    return
+                }
 
-    private func onMessage(message: URLSessionWebSocketTask.Message) { // 4
-        if case .string(let text) = message { // 5
-            print("받은 것 : \(text.data(using: .utf8))")
+            }
         }
-    }
     
     
     func send(text: String) {
@@ -57,11 +98,11 @@ final class RoomModel: ObservableObject {
 
 struct WaitingRoom: View {
     var roomData: RoomData
-    var is_admin = false
+    @State var is_admin = false
+    var team = "레드팀"
     @StateObject private var model = RoomModel()
     @Environment(\.presentationMode) var presentation
-    
-    
+
     private func onCommit(message: String) {
         model.send(text: message)
     }
@@ -139,40 +180,77 @@ struct WaitingRoom: View {
                 }
                 //플레이어 목록
                 HStack(alignment: .center, spacing: 3){
-                    HStack(){
-                        List(){
-                            HStack(){
-                                Text(Image(systemName: "crown.fill"))
-                                    .foregroundColor(Color.yellow)
-                                    .frame(width: gp.size.width*0.1)
-                                Text("MAD")
-                                    .frame(width: gp.size.width*0.2, alignment: .leading)
-                                Text(Image(systemName: "checkmark"))
-                                    .frame(width: gp.size.width*0.1, alignment: .center)
-                            }
-                            .frame(width: gp.size.width*0.47, alignment: .leading)
-                        }
-                        .frame(width: gp.size.width*0.47, height: gp.size.height / 3, alignment: .center)
-                    }
                     
                     HStack(){
                         List(){
-                            HStack(){
-                                Text(Image(systemName: "crown.fill"))
-                                    .foregroundColor(Color.yellow)
-                                    .frame(width: gp.size.width*0.1)
-                                Text("MAD")
-                                    .frame(width: gp.size.width*0.2, alignment: .leading)
-                                Text(Image(systemName: "checkmark"))
-                                    .frame(width: gp.size.width*0.1, alignment: .center)
+                            ForEach(0..<model.attend_user_list.count, id: \.self) { index in
+                                if model.attend_user_list[index].team == "레드팀" {
+                                    HStack(){
+                                        if let user_id: String = UserDefaults.standard.string(forKey: "user_id") {
+                                            if Int(user_id) == model.attend_user_list[index].user.id {
+                                                if model.attend_user_list[index].is_admin {
+                                                    Text(Image(systemName: "crown.fill"))
+                                                        .foregroundColor(Color.yellow)
+                                                        .frame(width: gp.size.width*0.1)
+                                                }
+                                                else {
+                                                    Text("")
+                                                        .frame(width: gp.size.width*0.1)
+                                                }
+                                            }
+                                            else {
+                                                Text("")
+                                                    .frame(width: gp.size.width*0.1)
+                                            }
+                                        }
+                                        Text(model.attend_user_list[index].user.call_sign)
+                                            .frame(width: gp.size.width*0.2, alignment: .leading)
+                                        Text(Image(systemName: "checkmark"))
+                                            .frame(width: gp.size.width*0.1, alignment: .center)
+                                    }
+                                    .frame(width: gp.size.width*0.47, alignment: .leading)
+                                }
                             }
-                            .frame(width: gp.size.width*0.47, alignment: .leading)
                             
+                        }
+                        .frame(width: gp.size.width*0.47, height: gp.size.height / 3, alignment: .center)
+                    }
+                    HStack(){
+                        List(){
+                            ForEach(0..<model.attend_user_list.count, id: \.self) { index in
+                                if model.attend_user_list[index].team == "블루팀" {
+                                    HStack(){
+                                        if let user_id: String = UserDefaults.standard.string(forKey: "user_id") {
+                                            if Int(user_id) == model.attend_user_list[index].user.id {
+                                                if model.attend_user_list[index].is_admin {
+                                                    Text(Image(systemName: "crown.fill"))
+                                                        .foregroundColor(Color.yellow)
+                                                        .frame(width: gp.size.width*0.1)
+                                                }
+                                                else {
+                                                    Text("")
+                                                        .frame(width: gp.size.width*0.1)
+                                                }
+                                            }
+                                            else {
+                                                Text("")
+                                                    .frame(width: gp.size.width*0.1)
+                                            }
+                                        }
+                                        Text(model.attend_user_list[index].user.call_sign)
+                                            .frame(width: gp.size.width*0.2, alignment: .leading)
+                                        Text(Image(systemName: "checkmark"))
+                                            .frame(width: gp.size.width*0.1, alignment: .center)
+                                    }
+                                    .frame(width: gp.size.width*0.47, alignment: .leading)
+                                }
+                            }
                         }
                         .frame(width: gp.size.width*0.47, height: gp.size.height / 3, alignment: .center)
                     }
                 }
                 .frame(width: gp.size.width, alignment: .leading)
+               
                 
                 Divider()
                 //옵저버
@@ -214,33 +292,54 @@ struct WaitingRoom: View {
                 //플레이어 목록
                 HStack(alignment: .center, spacing: 3){
                     List(){
-                        HStack(){
-                            Text(Image(systemName: "crown.fill"))
-                                .foregroundColor(Color.yellow)
-                                .frame(width: gp.size.width*0.15, alignment: .leading)
-                            Text("HERLOCK")
-                                .frame(width: gp.size.width*0.4, alignment: .leading)
-                            Text(Image(systemName: "checkmark"))
-                                .frame(width: gp.size.width*0.15, alignment: .center)
+                        ForEach(0..<model.attend_user_list.count, id: \.self) { index in
+                            if model.attend_user_list[index].team == "옵저버" {
+                                HStack(){
+                                    if let user_id: String = UserDefaults.standard.string(forKey: "user_id") {
+                                        if Int(user_id) == model.attend_user_list[index].user.id {
+                                            if model.attend_user_list[index].is_admin {
+                                                Text(Image(systemName: "crown.fill"))
+                                                    .foregroundColor(Color.yellow)
+                                                    .frame(width: gp.size.width*0.15, alignment: .leading)
+                                            }
+                                            else {
+                                                Text("")
+                                                    .frame(width: gp.size.width*0.15, alignment: .leading)
+                                            }
+                                        }
+                                        else {
+                                            Text("")
+                                                .frame(width: gp.size.width*0.15, alignment: .leading)
+                                        }
+                                    }
+                                    Text("HERLOCK")
+                                        .frame(width: gp.size.width*0.4, alignment: .leading)
+                                    Text(Image(systemName: "checkmark"))
+                                        .frame(width: gp.size.width*0.15, alignment: .center)
+                                }
+                                .frame(width: gp.size.width*0.8, alignment: .leading)
+                            }
                         }
-                        .frame(width: gp.size.width*0.8, alignment: .leading)
-                        
                     }
                     .frame(width: gp.size.width*0.8, height: gp.size.height / 8, alignment: .center)
                 }
                 .frame(width: gp.size.width, alignment: .center)
                 
-                
-                Button(action: {
-                    print("게임시작")
-                }){
-                    Text("게임시작")
-                        .frame(width: gp.size.width * 0.7, height: gp.size.height*0.1, alignment: .center)
-                        .background(Color.blue)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(5)
-                        .font(.system(size: 25))
+                VStack(){
+                    if self.is_admin == true {
+                        Button(action: {
+                            print("게임시작")
+                        }){
+                            Text("게임시작")
+                                .frame(width: gp.size.width * 0.7, height: gp.size.height*0.1, alignment: .center)
+                                .background(Color.blue)
+                                .foregroundColor(Color.white)
+                                .cornerRadius(5)
+                                .font(.system(size: 25))
+                        }
+                    }
                 }
+
             }
             .frame(width: gp.size.width, height: gp.size.height, alignment: .center)
         }
@@ -250,18 +349,19 @@ struct WaitingRoom: View {
             if self.is_admin == true { //방장이면
                 self.model.is_admin = "create"
                 if let user_id = UserDefaults.standard.string(forKey: "user_id") {
-                    dict = ["user": user_id, "team": "레드팀", "is_admin": "True"]
+                    dict = ["type": "user_attend", "user": user_id, "team": self.team, "is_admin": "True"]
                 }
             }
             else {
                 if let user_id = UserDefaults.standard.string(forKey: "user_id") {
-                    dict = ["user": user_id, "team": "레드팀"]
+                    dict = ["type": "user_attend", "user": user_id, "team": self.team]
                 }
             }
             self.model.connect()
             if let theJSONData = try? JSONSerialization.data(withJSONObject: dict, options: []) {
                 let theJSONText = String(data: theJSONData, encoding: .utf8)
                 print("JSON string = \(theJSONText!)")
+                model.send(text: theJSONText!)
             }
         })
         .onDisappear(perform: {
